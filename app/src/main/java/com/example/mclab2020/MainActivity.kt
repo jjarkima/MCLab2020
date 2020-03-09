c8f9f8b92f5bfb7e97b1b11c37c294ec2a4a0703
J <jjarkima@gmail.com>
2020-03-09 22:49:14 +0200

    Homework finished.

--- app/src/main/java/com/example/mclab2020/MainActivity.kt
+++ app/src/main/java/com/example/mclab2020/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.mclab2020
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
-
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -68,6 +77,30 @@ class MainActivity : AppCompatActivity() {
                     toast("No reminders yet")
                 }
             }
+        }
+    }
+
+    companion object {
+        val CHANNEL_ID="REMINDER_CHANNEL_ID"
+        var NotificationID=1567
+        fun showNotification(context: Context, message:String){
+            var notificationBuilder=NotificationCompat.Builder(context, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_search_24px)
+                    .setContentTitle("Reminder").setContentText(message)
+                    .setStyle(NotificationCompat.BigTextStyle().bigText(message))
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            var notificationManager=context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
+                val channel=NotificationChannel(
+                        CHANNEL_ID,
+                        context.getString(R.string.app_name),
+                        NotificationManager.IMPORTANCE_DEFAULT)
+                        .apply{description = context.getString(R.string.app_name)}
+                notificationManager.createNotificationChannel(channel)
+            }
+            val notification=NotificationID + Random(NotificationID).nextInt(1, 30)
+            notificationManager.notify(notification, notificationBuilder.build())
         }
     }
 }
--- app/src/main/java/com/example/mclab2020/ReminderReceiver.kt
+++ app/src/main/java/com/example/mclab2020/ReminderReceiver.kt
@@ -3,18 +3,26 @@ package com.example.mclab2020
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import androidx.room.Room
+import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.toast
 
 class ReminderReceiver : BroadcastReceiver() {
 
-
-
-
     override fun onReceive(context: Context, intent: Intent) {
 
-        val text = intent.getStringExtra("message")
-        context.toast(text!!)
+        val uid = intent.getIntExtra("uid", 0)
+        val text  = intent.getStringExtra("message")
 
+        //context.toast(text!!)
+
+        MainActivity.showNotification(context,text!!)
+
+        doAsync{
+            val db = Room.databaseBuilder(context, AppDatabase::class.java, "reminder").build()
+            db.reminderDao().delete(uid)
+            db.close()
+        }
     }
 
 
